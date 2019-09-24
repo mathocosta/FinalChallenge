4c3595759a24eb10eba5b48127b79c6ff82a1958
Paulo José <paulocardosob@gmail.com>
2019-09-24 10:29:15 -0300

    Implement color for dark mode

---
+++ Splay/Shared/Extensions/UIColor+customColors.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+customColors.swift
+//  Splay
+//
+//  Created by Paulo José on 24/09/19.
+//  Copyright © 2019 The Rest of Us. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var fadedRed: UIColor {
+      return UIColor(red: 223.0 / 255.0, green: 58.0 / 255.0, blue: 80.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var darkSlateBlue: UIColor {
+      return UIColor(red: 28.0 / 255.0, green: 33.0 / 255.0, blue: 88.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var backgroundColor: UIColor {
+        return UIColor(named: "backgroundColor")!
+    }
+    
+    @nonobjc class var textColor: UIColor {
+        return UIColor(named: "textColor")!
+    }
+    
+}
