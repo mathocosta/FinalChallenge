9182900b33d07798daff27a1810c877e1c050392
Martonio Junior <martoniojunior@hotmail.com>
2019-10-24 11:57:33 -0300

    Added time preferences

---
+++ Movinning/Shared/ExerciseTime.swift
@@ -0,0 +1,11 @@
+//
+//  ExerciseIntensity.swift
+//  Movinning
+//
+//  Created by Martônio Júnior on 24/10/19.
+//  Copyright © 2019 The Rest of Us. All rights reserved.
+//
+
+import Foundation
+
+enum ExerciseInte
